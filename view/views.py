30c4c0894858390063d581bf095d63963be0144f
Villian <yuyang.he@gatech.edu>
2016-11-14 10:10:16 -0500

    New view pages added.

--- view/views.py
+++ view/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, render
 from django.http import HttpResponse
 from django.template import RequestContext
 from view.models import MovieScore, MovieInfo
@@ -6,23 +6,21 @@ from view.models import MovieScore, MovieInfo
 
 # Create your views here.
 def add(request):
-    return render_to_response('sample.html')
+    # return render_to_response('sample.html', {'sss':1}, context_instance=RequestContext(request))
+    return render(request, 'sample.html')
 
 
-def show(request, view_id):
-    if view_id:
-        result = get_object_or_404(MovieInfo, id=view_id)
-    else:
-        newMovieInfo = MovieInfo(
-            request.POST["title"],
-            request.POST["genre"],
-            request.POST["actorID"],
-            request.POST["directorID"],
-            request.POST["length"]
-        )
+def show(request):
+    newMovieInfo = MovieInfo(
+        request.POST["title"],
+        request.POST["genre"],
+        request.POST["actorID"],
+        request.POST["directorID"],
+        request.POST["length"]
+    )
 
-        newMovieInfo.save()
-        result = get_object_or_404(MovieInfo, title=request.POST["title"]).id
+    newMovieInfo.save()
+    result = get_object_or_404(MovieInfo, title=request.POST["title"]).id
 
-    return render_to_response('index.html', {'result': result},
+    return render_to_response('sample.html', {'result': result},
                               context_instance=RequestContext(request))
